14bdac90207808efdea8c6f00f411106835a98fe
Madison Hung <51008147+madihung@users.noreply.github.com>
2023-08-23 21:18:13 -0700

    Delete project_e9s2b-master/src directory

--- project_e9s2b-master/src/main/model/FoodType.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-//Represents a category of food
-public enum FoodType {
-    ____________, Mexican, Chinese, Japanese, Italian, Malaysian, Canadian, Korean, Mediterranean, Fastfood,
-    Vietnamese, Greek, Healthy
-}
